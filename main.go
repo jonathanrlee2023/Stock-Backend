cf036b1a3cc4f7db7ef5ae200e3ad88c4c9ab798
jonathanrlee2023 <johnnyrlee05@gmail.com>
2025-01-02 23:41:44 -0600

    added data caching for earnings data

--- main.go
+++ main.go
@@ -152,18 +152,56 @@ func earningsHandler(w http.ResponseWriter, r *http.Request) {
 	ticker := r.URL.Query().Get("symbol")
 	alphaVantageApiKey := r.URL.Query().Get("apikey")
 
-	apiURL := fmt.Sprintf("https://www.alphavantage.co/query?function=EARNINGS&symbol=%s&apikey=%s", ticker, alphaVantageApiKey)
-	data, err := fetchAlphaVantageAPI(apiURL)
-	if err != nil {
-		http.Error(w, "Error fetching data", http.StatusInternalServerError)
+	// Define cache folder and file name
+	cacheFolder := "EarningsDataCache"
+	fileName := fmt.Sprintf("%searnings.json", ticker)
+	filePath := filepath.Join(cacheFolder, fileName)
+
+	// Ensure the cache folder exists
+	if err := os.MkdirAll(cacheFolder, 0755); err != nil {
+		http.Error(w, "Error creating cache folder", http.StatusInternalServerError)
 		return
 	}
-
+	// Check if the file exists
 	var result utils.EarningsResponse
+	if utils.FileExists(filePath) {
+		// Read the file and decode the data
+		fileData, err := os.ReadFile(filePath)
+		if err != nil {
+			http.Error(w, "Error reading cached data", http.StatusInternalServerError)
+			return
+		}
 
-	if err := json.Unmarshal(data, &result); err != nil {
-		http.Error(w, "Error parsing data", http.StatusInternalServerError)
-		return
+		if err := json.Unmarshal(fileData, &result); err != nil {
+			http.Error(w, "Error parsing cached data", http.StatusInternalServerError)
+			return
+		}
+		fmt.Println("Read data from cache")
+	} else {
+		// Fetch data from API
+		apiURL := fmt.Sprintf("https://www.alphavantage.co/query?function=EARNINGS&symbol=%s&apikey=%s", ticker, alphaVantageApiKey)
+		data, err := fetchAlphaVantageAPI(apiURL)
+		if err != nil {
+			http.Error(w, "Error fetching data", http.StatusInternalServerError)
+			return
+		}
+
+		if err := json.Unmarshal(data, &result); err != nil {
+			http.Error(w, "Error parsing data", http.StatusInternalServerError)
+			return
+		}
+
+		// Write the data to a file for caching
+		fileData, err := json.Marshal(result)
+		if err != nil {
+			http.Error(w, "Error saving cached data", http.StatusInternalServerError)
+			return
+		}
+
+		if err := os.WriteFile(filePath, fileData, 0644); err != nil {
+			http.Error(w, "Error writing cached data", http.StatusInternalServerError)
+			return
+		}
 	}
 
 	earningsData := result.QuarterlyEarnings
