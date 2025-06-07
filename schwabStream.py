1f05d3fa52d1d2f7e176de94d24a114fa4c95715
jonathanrlee2023 <johnnyrlee05@gmail.com>
2025-06-07 02:05:58 -0500

    add stock stream function

--- schwabStream.py
+++ schwabStream.py
@@ -3,6 +3,7 @@ import schwabdev
 from dotenv import load_dotenv
 import os
 import time
+import stream_func
 from datetime import datetime, timedelta
 
 
@@ -11,93 +12,31 @@ load_dotenv()  # loads variables from .env into environment
 appKey = os.getenv("appKey")
 appSecret = os.getenv("appSecret")
 
-field_labels = {
-    '0': 'Symbol',
-    '1': 'Description',
-    '2': 'Bid Price',
-    '3': 'Ask Price',
-    '4': 'Last Price',
-    '5': 'High Price',
-    '28': 'Delta',
-    '29': 'Gamma',
-    '30': 'Theta',
-    '31': 'Vega'
-}
 
 client = schwabdev.Client(app_key=appKey, app_secret=appSecret)
 
-streamer = client.stream
+streamer = client.stream 
 
-ticker = None
-option_price = None
-c_or_p = None
-option_id = None
-option_ids = []
-new_data = {}
-labeled_data = {}
-
-def start_stream():
-    global ticker
-    while True:
-        ticker = input("Enter ticker you want to see options for or 'Done' if you are done: ")
-        if ticker == 'Done':
-            break
-        while True:
-            c_or_p = input("Enter 'C' for call and 'P' for put or 'Done' if you are done: ")
-            if c_or_p == 'Done':
-                break
-            option_price = input('Enter option price: ')
-            strike_int = int(float(option_price) * 1000)
-            strike_str = str(strike_int).zfill(8)
-            day = input('Enter day of expiration: ')
-            month = input('Enter month of expiration: ')
-            year = input('Enter last two digits of year of expiration: ')
-            month_filled = month.zfill(2)
-            option_id = f'{year}{month_filled}{day}{c_or_p.upper()}{strike_str}'
-            streamer.send(streamer.level_one_options(f"{ticker}  {option_id}", "1,2,3,4,5,28,29,30,31", command='ADD'))
-            full_option_id = f"{ticker}_{option_id}"
-            option_ids.append(full_option_id)
-            file_path = f"{ticker}_{option_id}.json"
-            if not os.path.exists(file_path):
-                with open(file_path, "w") as f:
-                    json.dump({full_option_id: {}}, f)
-
-def receive_data(response):
-    parsed = json.loads(response)
-    
-    if 'data' in parsed:
-        for item in parsed['data']:
-            content = item.get("content", [])
-            
-
-            for quote in content:
-                symbol = quote.get("key")
-                symbol = symbol.replace('  ', '_')
-                if symbol not in new_data:
-                    new_data[symbol] = {}         
-                if symbol not in labeled_data:
-                    labeled_data[symbol] = {}           
-                for key, value in quote.items():
-                    if key in field_labels:
-                        labeled_data[symbol][field_labels[key]] = value
-                # Merge new data into stored state
-                new_data[symbol].update(labeled_data[symbol])                
 try:
     print("Starting stream...")
-    streamer.start_auto(receiver=receive_data)
+    streamer.start(receiver=stream_func.receive_data)
     
-    start_stream()
+    stream_func.start_options_stream(streamer=streamer)
 
     # Keep the script alive
     while True:
-        time.sleep(60)
-        for id in option_ids:
+        now = time.time()
+        sleep_seconds = 60 - (now % 60)
+        time.sleep(sleep_seconds)
+
+        timestamp = int(time.time())        
+        for names in stream_func.file_names:
             existing_data = None
             timestamp = int(time.time())
-            with open(f"{id}.json", "r") as f:
+            with open(f"{names}.json", "r") as f:
                 existing_data = json.load(f)
-            existing_data[id][timestamp] = new_data[id]
-            with open(f'{id}.json', "w") as f:
+            existing_data[timestamp] = stream_func.new_data[names]
+            with open(f'{names}.json', "w") as f:
                 json.dump(existing_data, f)
 except Exception as e:
     print("Stream error:", e)
---
+++ stream_func.py
@@ -0,0 +1,98 @@
+import json
+import os
+
+option_labels = {
+    '0': 'Symbol',
+    '1': 'Description',
+    '2': 'Bid Price',
+    '3': 'Ask Price',
+    '4': 'Last Price',
+    '5': 'High Price',
+    '28': 'Delta',
+    '29': 'Gamma',
+    '30': 'Theta',
+    '31': 'Vega'
+}
+
+stock_labels = {
+    '0': 'Symbol',
+    '1': 'Bid Price',
+    '2': 'Ask Price',
+    '3': 'Last Price',
+    '4': 'Bid Size',
+    '5': 'Ask Size'
+}
+ticker = None
+option_price = None
+c_or_p = None
+option_id = None
+file_names = []
+new_data = {}
+labeled_data = {}
+
+def start_options_stream(streamer):
+    global ticker
+    while True:
+        ticker = input("Enter ticker you want to see options for or 'Done' if you are done: ")
+        if ticker == 'Done':
+            break
+        while True:
+            c_or_p = input("Enter 'C' for call and 'P' for put or 'Done' if you are done: ")
+            if c_or_p == 'Done':
+                break
+            option_price = input('Enter option price: ')
+            strike_int = int(float(option_price) * 1000)
+            strike_str = str(strike_int).zfill(8)
+            day = input('Enter day of expiration: ')
+            month = input('Enter month of expiration: ')
+            year = input('Enter last two digits of year of expiration: ')
+            month_filled = month.zfill(2)
+            option_id = f'{year}{month_filled}{day}{c_or_p.upper()}{strike_str}'
+            streamer.send(streamer.level_one_options(f"{ticker}  {option_id}", "1,2,3,4,5,28,29,30,31", command='ADD'))
+            full_option_id = f"{ticker}_{option_id}"
+            file_names.append(full_option_id)
+            file_path = f"{ticker}_{option_id}.json"
+            if not os.path.exists(file_path):
+                with open(file_path, "w") as f:
+                    json.dump({}, f)
+
+def start_stock_stream(streamer):
+    global ticker
+    while True:
+        ticker = input("Enter ticker or 'Done' if you are done: ")
+        if ticker == 'Done':
+            break
+        caps_ticker = ticker.upper()
+        streamer.send(streamer.level_one_equities(f"{caps_ticker}", "1,2,3,4,5", command='ADD'))
+        file_names.append(caps_ticker)
+        file_path = f"{caps_ticker}.json"
+        if not os.path.exists(file_path):
+            with open(file_path, "w") as f:
+                json.dump({}, f)
+
+def receive_data(response):
+    parsed = json.loads(response)
+    
+    if 'data' in parsed:
+        for item in parsed['data']:
+            content = item.get("content", [])
+            
+
+            for quote in content:
+                symbol = quote.get("key")
+                if '  ' in symbol:
+                    symbol = symbol.replace('  ', '_')
+                if symbol not in new_data:
+                    new_data[symbol] = {}         
+                if symbol not in labeled_data:
+                    labeled_data[symbol] = {}
+                if len(symbol) > 5:           
+                    for key, value in quote.items():
+                        if key in option_labels:
+                            labeled_data[symbol][option_labels[key]] = value
+                else:
+                    for key, value in quote.items():
+                        if key in stock_labels:
+                            labeled_data[symbol][stock_labels[key]] = value
+                # Merge new data into stored state
+                new_data[symbol].update(labeled_data[symbol]) 
